c14f85f6637c9a49d6e1876ace8e69f970e1213e
iam-raja512 <89579256+iam-raja512@users.noreply.github.com>
2026-08-16 17:43:27 +0530

    Created project

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.59.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "terraform"
+}
+
+resource "aws_instance" "server" {
+ami= "ami-0011550b539717e2a"
+instance_type = "t3.micro"
+availability_zone = "ap-south-1a" 
+key_name = "AmazonWebservices"
+
+
+
+tags = {
+    Name = "Terraform_CICD"
+}
+}
